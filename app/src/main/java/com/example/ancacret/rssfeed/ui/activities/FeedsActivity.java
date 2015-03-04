ca188d2e0039c0001e40b40275a598ae8774ba19
Anca Cret <ancacret025@gmail.com>
2015-03-04 16:33:27 +0200

    Added comment to see how commit works

--- app/src/main/java/com/example/ancacret/rssfeed/ui/activities/FeedsActivity.java
+++ app/src/main/java/com/example/ancacret/rssfeed/ui/activities/FeedsActivity.java
@@ -671,6 +671,8 @@ public class FeedsActivity extends ActionBarActivity implements IOnTaskCompleted
         }
     }
 
+    /*test comment*/
+
     /* check if current category equals the last category of the provider */
     private boolean isLastCategory() {
         Set set = mCurrentProvider.getCategoryObjects().keySet();
